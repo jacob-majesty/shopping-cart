f400b222ad88781cb7f9c215e3bbb7c5733517bd
“[jacob-majesty]” <[jacobamony@gmail.com]>
2024-11-24 02:12:11 -0300

    Added Mode: Category, image and Product classes

---
+++ src/main/java/com/majesty/shop/model/Image.java
@@ -0,0 +1,29 @@
+package com.majesty.shop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.sql.Blob;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String filename;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn (name = "product_id")
+    private Product product;
+}
